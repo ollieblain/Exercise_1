c08ee18a3b0fb0dd8608fe9a25dfceb83586122a
oliver.blain <oliver.blain@ASAZ-F8VZ2M3>
2022-11-07 10:08:42 +0000

    Add project files.

---
+++ Exercise_1/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    internal class Person
+    {
+        string firstName;
+        string lastName;
+        string userDOB;
+        DateTime date;
+
+        //create constructor
+
+        public Person(string firstName, string lastName, string userDOB)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.userDOB = userDOB;
+
+        }
+        public string getDay() //using in built method DayOfWeek. Convert to string 
+        {
+            return date.DayOfWeek.ToString();
+        }
+
+        public string getFirstName()
+        {
+            return this.firstName;
+        }
+
+        public string getLastName()
+        {
+            return this.lastName;
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Exercise_1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your date of birth in the format dd-mm-yyyy");
+            string userDOB = Console.ReadLine();
+
+            //output all variables
+        }
+    }
+}
